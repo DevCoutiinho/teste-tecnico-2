8ffe2f54511bc70ba1d4823c4e29b9257ff19417
Marcos Coutinho <veniciusm4@gmail.com>
2025-04-02 23:51:40 -0300

    Melhorando o componente spinner

--- src/componentes/Spinner.jsx
+++ src/componentes/Spinner.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+  import { LoaderCircle } from "lucide-react";
 
 const Spinner = () => {
   return (
-    <div className=" mt-9 w-[40px] h-[40px] rounded-full border-2 border-gray-200 border-t-blue-500 animate-spin animate-infinite animate-ease-linear animate-normal"></div>
+    <LoaderCircle size={40} className=" mt-9  text-blue-500 animate-spin animate-infinite animate-ease-linear animate-normal"/>
   );
 };
 
